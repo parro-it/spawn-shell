836179fc417faa5f3b4c83ffac9d1e4d79b44124
Andrea Parodi <andrea@parro.it>
2017-03-29 16:30:55 +0200

    ava + xo

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 'use strict';
 
 const childProcess = require('child_process');
-const spawn = childProcess.spawn;
 const defaultShell = require('default-shell');
 const merge = require('merge-options');
 const npmRunPath = require('npm-run-path');
 
+const spawn = childProcess.spawn;
 
 const defaultOptions = {
-  env: {},
-  stdio: [0, 1, 2],
-  windowsVerbatimArguments: process.platform === 'win32'
+	env: {},
+	stdio: [0, 1, 2],
+	windowsVerbatimArguments: process.platform === 'win32'
 };
 
+module.exports = spawnShell;
+
 function shellFlags() {
-  if (process.platform === 'win32') {
-    return ['/d', '/s', '/c'];
-  }
-  return ['-c'];
+	if (process.platform === 'win32') {
+		return ['/d', '/s', '/c'];
+	}
+	return ['-c'];
 }
 
 function resolveOnProcessExit(p) {
-  return new Promise((resolve, reject) => {
-    let fullFilled = false;
-    p.on('error', err => {
-      fullFilled = true;
-      reject(err);
-    });
+	return new Promise((resolve, reject) => {
+		let fullFilled = false;
+		p.on('error', err => {
+			fullFilled = true;
+			reject(err);
+		});
 
-    p.on('exit', exitCode => {
-      if (!fullFilled) {
-        resolve(exitCode);
-      }
-    });
-  });
+		p.on('exit', exitCode => {
+			if (!fullFilled) {
+				resolve(exitCode);
+			}
+		});
+	});
 }
 
-module.exports = function spawnShell(command, options) {
-  const opts = merge({}, defaultOptions, options);
-  const shell = opts.shell || defaultShell;
-  delete opts.shell;
-  opts.env.PATH = npmRunPath({path: opts.env.PATH});
+function spawnShell(command, options) {
+	const opts = merge({}, defaultOptions, options);
+	const shell = opts.shell || defaultShell;
+	delete opts.shell;
+	opts.env.PATH = npmRunPath({path: opts.env.PATH});
 
-  const p = spawn(
-    shell,
-    shellFlags().concat(command),
-    opts
-  );
-  p.exitPromise = resolveOnProcessExit(p);
-  return p;
-};
+	const p = spawn(
+		shell,
+		shellFlags().concat(command),
+		opts
+	);
+	p.exitPromise = resolveOnProcessExit(p);
+	return p;
+}
--- test.js
+++ test.js
@@ -1,51 +1,35 @@
-const test = require('tape');
-const spawnShell = require('./');
-const concat = require('stream-string');
-const co = require('co');
+import test from 'ava';
+import concat from 'stream-string';
+import spawnShell from './';
 
-test('Use shell to run commands', co.wrap(function * (t) {
+test('Use shell to run commands', async t => {
+	const p = spawnShell('echo "it works"', {
+		stdio: [0, 'pipe', 2]
+	});
 
-  const p = spawnShell('echo "it works"', {
-    stdio: [0, 'pipe', 2]
-  });
-
-  const output = yield concat(p.stdout);
-  t.equal(output, 'it works\n');
-  t.end();
-
-}));
-
-test('Returned ChildProcess has a exitPromise prop that resolve with process exit code', co.wrap(function * (t) {
-
-  const p = spawnShell('exit 1');
-  const exitCode = yield p.exitPromise;
-  t.equal(exitCode, 1);
-  t.end();
-
-}));
-
-
-test('Returned ChildProcess exitPromise is rejected on spawn errors', t => {
-
-  const p = spawnShell('', { shell: 'unknown' });
-
-  p.exitPromise.catch(err => {
-    t.equal(err.code, 'ENOENT');
-    t.end();
-  });
-
+	const output = await concat(p.stdout);
+	t.is(output, 'it works\n');
 });
 
+test('Returned ChildProcess has a exitPromise prop that resolve with process exit code', async t => {
+	const p = spawnShell('exit 1');
+	const exitCode = await p.exitPromise;
+	t.is(exitCode, 1);
+});
 
-test('inject your package `node_modules/.bin` directory in path', co.wrap(function * (t) {
+test('Returned ChildProcess exitPromise is rejected on spawn errors', async t => {
+	const p = spawnShell('', {shell: 'unknown'});
 
-  const p = spawnShell('which eslint', {
-    stdio: [0, 'pipe', 2],
-    shell: 'sh'
-  });
+	const err = await t.throws(p.exitPromise);
+	t.is(err.code, 'ENOENT');
+});
 
-  const output = yield concat(p.stdout);
-  t.equal(output, __dirname + '/node_modules/.bin/eslint\n');
-  t.end();
+test('inject your package `node_modules/.bin` directory in path', async t => {
+	const p = spawnShell('which eslint', {
+		stdio: [0, 'pipe', 2],
+		shell: 'sh'
+	});
 
-}));
+	const output = await concat(p.stdout);
+	t.is(output, `${__dirname}/node_modules/.bin/eslint\n`);
+});
